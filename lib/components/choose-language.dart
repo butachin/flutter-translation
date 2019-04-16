1ba634033492e01168e60c91d5cd2c2a0f657f75
若林勇汰 <yufo.yuta0520@gmail.com>
2019-04-16 09:16:17 +0900

    [update]

--- lib/components/choose-language.dart
+++ lib/components/choose-language.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 
+import '../models/language.dart';
+import '../screens/language-page.dart';
+
 class ChooseLanguage extends StatefulWidget {
-  ChooseLanguage({Key key}) : super(key: key);
+  ChooseLanguage({Key key, this.onLanguageChanged}) : super(key: key);
+
+  final Function(Language firstCode, Language secondCode) onLanguageChanged;
 
   @override
   _ChooseLanguageState createState() => _ChooseLanguageState();
 }
 
 class _ChooseLanguageState extends State<ChooseLanguage> {
-  String _firstLanguage = "English";
-  String _secondLanguage = "French";
+  Language _firstLanguage = Language('en', 'English', true, true, true);
+  Language _secondLanguage = Language('fr', 'French', true, true, true);
+
+  // Switch the first and the second language
+  void _switchLanguage() {
+    Language _tmpLanguage = this._firstLanguage;
+
+    setState(() {
+      this._firstLanguage = this._secondLanguage;
+      this._secondLanguage = _tmpLanguage;
+    });
+
+    this.widget.onLanguageChanged(this._firstLanguage, this._secondLanguage);
+  }
+
+  // Choose a new first language
+  void _chooseFirstLanguage(String title, bool isAutomaticEnabled) async {
+    final language = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LanguagePage(
+          title: title,
+          isAutomaticEnabled: isAutomaticEnabled,
+        ),
+      ),
+    );
+
+    if (language != null) {
+      this.setState(() {
+        this._firstLanguage = language;
+      });
+
+      this.widget.onLanguageChanged(this._firstLanguage, this._secondLanguage);
+    }
+  }
+
+  // Choose a new second language
+  void _chooseSecondLanguage(String title, bool isAutomaticEnabled) async {
+    final language = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LanguagePage(
+          title: title,
+          isAutomaticEnabled: isAutomaticEnabled,
+        ),
+      ),
+    );
+
+    if (language != null) {
+      this.setState(() {
+        this._secondLanguage = language;
+      });
+
+      this.widget.onLanguageChanged(this._firstLanguage, this._secondLanguage);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +91,14 @@ class _ChooseLanguageState extends State<ChooseLanguage> {
             child: Material(
               color: Colors.white,
               child: InkWell(
-                onTap: () {},
+                onTap: () {
+                  this._chooseFirstLanguage("Translate from", true);
+                },
                 child: Center(
                   child: Text(
-                    this._firstLanguage,
+                    this._firstLanguage.name,
                     style: TextStyle(
-                      color: Colors.orange[600],
+                      color: Colors.blue[600],
                       fontSize: 15.0,
                     ),
                   ),
@@ -52,19 +113,21 @@ class _ChooseLanguageState extends State<ChooseLanguage> {
                 Icons.compare_arrows,
                 color: Colors.grey[700],
               ),
-              onPressed: () {},
+              onPressed: this._switchLanguage,
             ),
           ),
           Expanded(
             child: Material(
               color: Colors.white,
               child: InkWell(
-                onTap: () {},
+                onTap: () {
+                  this._chooseSecondLanguage("Translate to", false);
+                },
                 child: Center(
                   child: Text(
-                    this._secondLanguage,
+                    this._secondLanguage.name,
                     style: TextStyle(
-                      color: Colors.orange[600],
+                      color: Colors.blue[600],
                       fontSize: 15.0,
                     ),
                   ),
